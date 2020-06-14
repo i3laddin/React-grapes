17aaed2ae12aa5adfe4d45667e2c656c23a9a415
mohammed aladdin <i3laddin@gmail.com>
2020-06-14 08:14:45 +0200

    need to fix mapping function

--- src/card/Material.js
+++ src/card/Material.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { type } from "os";
 
 
 const data = [
@@ -6,8 +7,8 @@ const data = [
         nameAR: "المملكة العربية السعودية",
         nameEN: "Saudi Arabia",
         id: "5c24dc26dc10670017e40921",
-        createdAt: "2020-06-10T19:51:40.452Z",
-        updatedAt: "2020-06-10T19:51:40.452Z"
+        //createdAt: "2020-06-10T19:51:40.452Z",
+        //updatedAt: "2020-06-10T19:51:40.452Z"
     },
     {
         nameAR: "مصر",
@@ -15,47 +16,64 @@ const data = [
         id: "5d8b1de47c213e556135d9d2",
         createdAt: "2020-06-10T19:51:40.453Z",
         updatedAt: "2020-06-10T19:51:40.453Z"
+    }, {
+        hi: "hi"
     }
 ]
-console.log(data)
+
+
+
+//const arr = [{ x: 1, y: 2 }, { x: 5, y: 4 }, { x: 1, y: 2, z: 3 }];
 
 const Material = (props) => {
+    //const {data}= props
 
-        
-    console.log(props.data)
+    function getValue(obj, key) {
+        return obj[key];
+    }
+    const keys = [...new Set([].concat.apply([], props.data.map(obj => Object.keys(obj))))];
+    
+  
+
+    for (let i = 0, j = 0; i < props.data.length && j < 10; i++, j++) {
+
+        const kee = Object.keys(props.data[i])
+        console.log(kee)
+        const keeStr = kee.toString();
+        console.log(keeStr)
+        for (let j = 0; j < kee.length; j++) {
+            const keyString = keeStr.match(kee[j])[0]
+            console.log(keyString)
+            console.log(getValue({ ...props.data[0] }, keyString))
+        }
+    } 
+   
     return (
         <table border="1" cellspacing="0" cellpadding="0" >
-                        
+
             <tbody>
-                            <tr >
-                                <th>nameAR</th>
-                                <th>nameEN</th>
-                                <th>id</th>
-                                <th>createdAt</th>
-                                <th>updatedAt</th>
+                <tr>
+                    {keys.map((key) => {
+                        return (
+                            <th>{key}</th>
+                        )
+                    })}
                 </tr>
                 {props.data.map((dt) => {
-                    return (
-                        <tr>
-                            <td>{dt.nameAR}</td>
-                            <td>{dt.nameEN}</td>
-                            <td>{dt.id}</td>
-                            <td>{dt.createdAt}</td>
-                            <td>{dt.createdAt}</td>
-
-                        </tr>
-                    )
+                return (
+                        console.log(dt)
+                )
                 })}
-                            
 
 
-                    
-                        </tbody>
-                </table> 
-                            
-            
-        );
-    
+
+
+            </tbody>
+        </table>
+
+
+    );
+
 }
 
 export default Material
